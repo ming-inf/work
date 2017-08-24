fdcf56dd3dbfc3dc2a2181ecf4258a4b57a2f0fe
Ming Wong <ming.wong@rbccm.com>
2017-08-24 15:52:17 -0400

    use reflection to get char[] from StringBuilder

--- code/misc/src/main/java/javafx/scene/control/SafePasswordField.java
+++ code/misc/src/main/java/javafx/scene/control/SafePasswordField.java
@@ -1,20 +1,22 @@
 package javafx.scene.control;
-import java.lang.reflect.Field;
 
-import javafx.scene.control.PasswordField;
+import java.lang.reflect.Field;
 
 public class SafePasswordField extends PasswordField {
 	public final char[] getPassword()
 			throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
 		Content c = getContent();
 
-		Field fld = c.getClass().getDeclaredField("characters");
-		fld.setAccessible(true);
-
-		StringBuilder sb = (StringBuilder) fld.get(c);
-		char[] result = new char[sb.length()];
-		sb.getChars(0, sb.length(), result, 0);
+		StringBuilder sb = (StringBuilder) getFieldReflection(c, c.getClass(), "characters");
+		char[] result = (char[]) getFieldReflection(sb, sb.getClass().getSuperclass(), "value");
 
 		return result;
+	}
+
+	private Object getFieldReflection(Object o, Class<?> c, String field)
+			throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+		Field f = c.getDeclaredField(field);
+		f.setAccessible(true);
+		return f.get(o);
 	}
 }
