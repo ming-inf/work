b2ce8524329bca30d453d85626992b5210a4b69e
Ming Wong <ming.wong@rbccm.com>
2017-09-13 09:49:19 -0400

    add test suite to run all tests

---
+++ code/misc/src/test/java/structure/AllTestsSuite.java
@@ -0,0 +1,14 @@
+package structure;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({
+	SinglyLinkedListTest.class
+	, DoublyLinkedListTest.class
+})
+
+public class AllTestsSuite {
+}
