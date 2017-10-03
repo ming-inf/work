871b3bb6804e256c8c4d6f157aa8796d2e19d7ff
Ming Wong <ming.wong@rbccm.com>
2017-10-03 08:54:12 -0400

    remove test inheritance

--- code/misc/src/test/java/structure/AVLTreeTest.java
+++ code/misc/src/test/java/structure/AVLTreeTest.java
@@ -5,39 +5,145 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
 
-public class AVLTreeTest extends TreeTest {
+public class AVLTreeTest {
+	private Tree<Integer> objectUnderTest;
+
 	@Before
 	public void setup() {
 		objectUnderTest = new AVLTree<>();
 	}
 
 	@Test
-	public void testInsertAvlLeftRotate() {
-		objectUnderTest.add(1);
-		objectUnderTest.add(2);
-		objectUnderTest.add(3);
-		objectUnderTest.add(4);
-		objectUnderTest.add(5);
-		objectUnderTest.add(6);
-
-		assertEquals(3, objectUnderTest.height());
+	public void testSearchNull() {
+		assertFalse(objectUnderTest.contains(null));
 	}
 
 	@Test
-	public void testInsertAvlRightRotate() {
-		objectUnderTest.add(6);
-		objectUnderTest.add(5);
-		objectUnderTest.add(4);
-		objectUnderTest.add(3);
-		objectUnderTest.add(2);
+	public void testSearchNotFoundEmpty() {
+		assertFalse(objectUnderTest.contains(0));
+	}
+
+	@Test
+	public void testSearchNotFound() {
+		objectUnderTest.add(0);
+
+		assertFalse(objectUnderTest.contains(1));
+	}
+
+	@Test
+	public void testSearchFound() {
+		objectUnderTest.add(0);
+
+		assertTrue(objectUnderTest.contains(0));
+	}
+
+	@Test
+	public void testSearchNotFoundLeft() {
+		objectUnderTest.add(0);
+		objectUnderTest.add(-1);
+
+		assertFalse(objectUnderTest.contains(-2));
+	}
+
+	@Test
+	public void testSearchNotFoundRight() {
+		objectUnderTest.add(0);
 		objectUnderTest.add(1);
 
-		assertEquals(3, objectUnderTest.height());
+		assertFalse(objectUnderTest.contains(2));
+	}
+
+	@Test
+	public void testSearchNotFoundBoth() {
+		objectUnderTest.add(0);
+		objectUnderTest.add(-1);
+		objectUnderTest.add(1);
+
+		assertFalse(objectUnderTest.contains(-2));
+	}
+
+	@Test
+	public void testInsertNull() {
+		objectUnderTest.add(null);
+	}
+
+	@Test
+	public void testInsert() {
+		objectUnderTest.add(0);
+
+		assertFalse(objectUnderTest.contains(-1));
+		assertTrue(objectUnderTest.contains(0));
+		assertFalse(objectUnderTest.contains(1));
+	}
+
+	@Test
+	public void testDeleteNull() {
+		assertFalse(objectUnderTest.remove(null));
+	}
+
+	@Test
+	public void testDeleteEmpty() {
+		assertFalse(objectUnderTest.remove(0));
+	}
+
+	@Test
+	public void testDeleteNotFound() {
+		objectUnderTest.add(0);
+
+		assertFalse(objectUnderTest.remove(1));
+	}
+
+	@Test
+	public void testDeleteRootLeaf() {
+		objectUnderTest.add(0);
+
+		assertTrue(objectUnderTest.remove(0));
+		assertFalse(objectUnderTest.contains(0));
+	}
+
+	@Test
+	public void testDeleteRootWithLeft() {
+		objectUnderTest.add(0);
+		objectUnderTest.add(-1);
+
+		assertTrue(objectUnderTest.remove(0));
+		assertFalse(objectUnderTest.contains(0));
+	}
+
+	@Test
+	public void testDeleteRootWithRight() {
+		objectUnderTest.add(0);
+		objectUnderTest.add(1);
+
+		assertTrue(objectUnderTest.remove(0));
+		assertFalse(objectUnderTest.contains(0));
+	}
+
+	@Test
+	public void testDeleteRootWithBoth() {
+		objectUnderTest.add(0);
+		objectUnderTest.add(1);
+		objectUnderTest.add(-1);
+
+		assertTrue(objectUnderTest.remove(0));
+		assertFalse(objectUnderTest.contains(0));
+		assertTrue(objectUnderTest.contains(1));
+		assertTrue(objectUnderTest.contains(-1));
+	}
+
+	@Test
+	public void testDeleteNonRootLeftLeaf() {
+		objectUnderTest.add(0);
+		objectUnderTest.add(-1);
+
+		assertTrue(objectUnderTest.remove(-1));
+		assertFalse(objectUnderTest.contains(-1));
 	}
 
 	@Test
@@ -76,6 +182,15 @@ public class AVLTreeTest extends TreeTest {
 		assertFalse(objectUnderTest.contains(-2));
 		assertTrue(objectUnderTest.contains(-1));
 		assertTrue(objectUnderTest.contains(-3));
+	}
+
+	@Test
+	public void testDeleteNonRootRightLeaf() {
+		objectUnderTest.add(0);
+		objectUnderTest.add(1);
+
+		assertTrue(objectUnderTest.remove(1));
+		assertFalse(objectUnderTest.contains(1));
 	}
 
 	@Test
@@ -133,28 +248,6 @@ public class AVLTreeTest extends TreeTest {
 	}
 
 	@Test
-	public void testLeftRightRotation() {
-		objectUnderTest.add(0);
-		objectUnderTest.add(1);
-		objectUnderTest.add(-2);
-		objectUnderTest.add(-1);
-
-		assertTrue(objectUnderTest.remove(1));
-		assertFalse(objectUnderTest.contains(1));
-	}
-
-	@Test
-	public void testRightLeftRotation() {
-		objectUnderTest.add(0);
-		objectUnderTest.add(-1);
-		objectUnderTest.add(2);
-		objectUnderTest.add(1);
-
-		assertTrue(objectUnderTest.remove(-1));
-		assertFalse(objectUnderTest.contains(-1));
-	}
-
-	@Test
 	public void testTraversePreorder() {
 		Integer[] intArray = { 23, 14, 7, 9, 17, 31 };
 		List<Integer> ints = Arrays.asList(intArray);
@@ -200,6 +293,14 @@ public class AVLTreeTest extends TreeTest {
 	}
 
 	@Test
+	public void testTraverseBreadthFirstNull() {
+		List<Integer> expected = Collections.emptyList();
+		List<Integer> actualIntArray = objectUnderTest.breadthFirst();
+
+		assertEquals(expected, actualIntArray);
+	}
+
+	@Test
 	public void testTraverseBreadthFirst() {
 		Integer[] intArray = { 23, 14, 7, 9, 17, 31 };
 		List<Integer> ints = Arrays.asList(intArray);
@@ -226,4 +327,51 @@ public class AVLTreeTest extends TreeTest {
 
 		assertEquals(3, objectUnderTest.height());
 	}
+
+	@Test
+	public void testInsertAvlLeftRotate() {
+		objectUnderTest.add(1);
+		objectUnderTest.add(2);
+		objectUnderTest.add(3);
+		objectUnderTest.add(4);
+		objectUnderTest.add(5);
+		objectUnderTest.add(6);
+
+		assertEquals(3, objectUnderTest.height());
+	}
+
+	@Test
+	public void testInsertAvlRightRotate() {
+		objectUnderTest.add(6);
+		objectUnderTest.add(5);
+		objectUnderTest.add(4);
+		objectUnderTest.add(3);
+		objectUnderTest.add(2);
+		objectUnderTest.add(1);
+
+		assertEquals(3, objectUnderTest.height());
+	}
+
+	@Test
+	public void testLeftRightRotation() {
+		objectUnderTest.add(0);
+		objectUnderTest.add(1);
+		objectUnderTest.add(-2);
+		objectUnderTest.add(-1);
+
+		assertTrue(objectUnderTest.remove(1));
+		assertFalse(objectUnderTest.contains(1));
+	}
+
+	@Test
+	public void testRightLeftRotation() {
+		objectUnderTest.add(0);
+		objectUnderTest.add(-1);
+		objectUnderTest.add(2);
+		objectUnderTest.add(1);
+
+		assertTrue(objectUnderTest.remove(-1));
+		assertFalse(objectUnderTest.contains(-1));
+	}
+
 }
