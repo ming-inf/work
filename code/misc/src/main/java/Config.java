1ea10b596dbe43ef5b88f37a0ce53016b5e3f7e2
Ming Wong <ming.wong@rbccm.com>
2017-10-12 09:18:35 -0400

    add yaml configuration

---
+++ code/misc/src/main/java/Config.java
@@ -0,0 +1,18 @@
+
+public class Config {
+	private String locale;
+	private String stylesheet;
+
+	public String getLocale() {
+		return locale;
+	}
+	public void setLocale(String locale) {
+		this.locale = locale;
+	}
+	public String getStylesheet() {
+		return stylesheet;
+	}
+	public void setStylesheet(String stylesheet) {
+		this.stylesheet = stylesheet;
+	}
+}
--- code/misc/src/main/java/Main.java
+++ code/misc/src/main/java/Main.java
@@ -1,9 +1,18 @@
 import java.io.IOException;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import com.fasterxml.jackson.dataformat.yaml.YAMLGenerator.Feature;
+
 import javafx.App;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
+		ObjectMapper mapper = new ObjectMapper(new YAMLFactory().enable(Feature.MINIMIZE_QUOTES));
+
+		Config config = mapper.readValue(Main.class.getResource("config.yaml"), Config.class);
+		mapper.writeValue(System.out, config);
+
 		App app = new App();
 		app.run(args);
 	}
