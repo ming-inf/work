e5782808f00673035cedc029ad9276a8e2e8b5d2
Ming Wong <ming.wong@rbccm.com>
2017-11-08 15:57:39 -0500

    add levenshtein distance using trie

---
+++ code/misc/src/main/java/algorithm/LevenshteinWithTrie.java
@@ -0,0 +1,67 @@
+package algorithm;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class LevenshteinWithTrie {
+	TrieNode root = new TrieNode();
+
+	public void insert(String word) {
+		root.insert(word);
+	}
+
+	public Map<String, Integer> search(String word, int maxCost) {
+		Map<String, Integer> results = new HashMap<>();
+
+		java.util.List<Integer> currentRow = IntStream.range(0, word.length() + 1).boxed().collect(Collectors.toList());
+
+		for (char letter : root.children.keySet()) {
+			searchRecursive(root.children.get(letter), letter, word, currentRow, results, maxCost);
+		}
+
+		return results;
+	}
+
+	public void searchRecursive(TrieNode node, char letter, String word, java.util.List<Integer> previousRow, Map<String, Integer> results, int maxCost) {
+		int columns = word.length() + 1;
+		java.util.List<Integer> currentRow = new ArrayList<>();
+		currentRow.add(previousRow.get(0) + 1);
+
+		for (int column = 1; column < columns; column++) {
+			int insertCost = currentRow.get(column - 1) + 1;
+			int deleteCost = previousRow.get(column) + 1;
+			int replaceCost = previousRow.get(column - 1) + ((letter != word.charAt(column - 1) ? 1 : 0));
+			currentRow.add(Math.min(insertCost, Math.min(deleteCost, replaceCost)));
+		}
+
+		if (currentRow.get(currentRow.size() - 1) <= maxCost && node.word != null) {
+			results.put(node.word, currentRow.get(currentRow.size() - 1));
+		}
+
+		if (Collections.min(currentRow) <= maxCost) {
+			for (char c : node.children.keySet()) {
+				searchRecursive(node.children.get(c), c, word, currentRow, results, maxCost);
+			}
+		}
+	}
+}
+
+class TrieNode {
+	String word;
+	Map<Character, TrieNode> children = new HashMap<>();
+
+	public void insert(String word) {
+		TrieNode node = this;
+		for (char letter : word.toCharArray()) {
+			if (!node.children.containsKey(letter)) {
+				node.children.put(letter, new TrieNode());
+			}
+			node = node.children.get(letter);
+		}
+		node.word = word;
+	}
+}
---
+++ code/misc/src/test/java/algorithm/LevenshteinWithTrieTest.java
@@ -0,0 +1,24 @@
+package algorithm;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class LevenshteinWithTrieTest {
+	LevenshteinWithTrie objectUnderTest;
+
+	@Test
+	public void testLevenshteinWithTrie() {
+		objectUnderTest = new LevenshteinWithTrie();
+
+		String wordsString = "a an as and ant";
+		java.util.List<String> words = Arrays.asList(wordsString.split(" "));
+		words.forEach(objectUnderTest::insert);
+
+		Set<String> expected = new HashSet<>(Arrays.asList(wordsString.split(" ")));
+		Assert.assertEquals(expected, objectUnderTest.search("an", 1).keySet());
+	}
+}
