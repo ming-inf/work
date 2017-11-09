422086d53c35ba811d1ec77010fea77c358221e9
Ming Wong <ming.wong@rbccm.com>
2017-11-09 15:33:18 -0500

    add trie

---
+++ code/misc/src/main/java/structure/Trie.java
@@ -0,0 +1,28 @@
+package structure;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Trie {
+	TrieNode trie = new TrieNode();
+
+	public void insert(String word) {
+		trie.insert(word);
+	}
+}
+
+class TrieNode {
+	String word;
+	Map<Character, TrieNode> children = new HashMap<>();
+
+	public void insert(String word) {
+		TrieNode node = this;
+		for (char letter : word.toCharArray()) {
+			if (!children.containsKey(letter)) {
+				node.children.put(letter, new TrieNode());
+			}
+			node = node.children.get(letter);
+		}
+		node.word = word;
+	}
+}
---
+++ code/misc/src/test/java/structure/TrieTest.java
@@ -0,0 +1,18 @@
+package structure;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
+public class TrieTest {
+	Trie objectUnderTest;
+
+	@Test
+	public void testTrie() {
+		objectUnderTest = new Trie();
+
+		String wordsString = "a an as and ant";
+		java.util.List<String> words = Arrays.asList(wordsString.split(" "));
+		words.forEach(objectUnderTest::insert);
+	}
+}
