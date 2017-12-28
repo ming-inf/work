754970ef70e2425a5d3fd8633dfb2ab25db472b5
Ming Wong <ming.wong@rbccm.com>
2017-12-28 12:35:46 -0500

    remove System.out.println

--- code/misc/src/test/java/structure/GraphTest.java
+++ code/misc/src/test/java/structure/GraphTest.java
@@ -75,7 +75,6 @@ public class GraphTest {
 
 		String wordsString = "0 a\n1 b\n2 c\n\n0 2\n1 2";
 		objectUnderTest.fromString(wordsString);
-		System.out.println(objectUnderTest.toString());
 	}
 
 	@Test
@@ -84,7 +83,6 @@ public class GraphTest {
 
 		String wordsString = "0 a\n1 b\n2 c\n3 d\n4 e\n5 f\n6 g\n\n0 1\n0 2\n1 3\n4 1\n6 4\n5 6\n5 2\n6 0";
 		objectUnderTest.fromString(wordsString);
-		System.out.println(objectUnderTest.toString());
 	}
 
 	@Test
@@ -93,6 +91,5 @@ public class GraphTest {
 
 		String wordsString = "0 foo\n1 bar\n2 bat\n\n0 1\n0 2\n1 2";
 		objectUnderTest.fromString(wordsString);
-		System.out.println(objectUnderTest.toString());
 	}
 }
