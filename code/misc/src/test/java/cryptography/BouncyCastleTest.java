cd2dc7a2208798e2474be8efe600b79b22d00fac
Ming Wong <ming.wong@rbccm.com>
2018-01-05 11:40:20 -0500

    add scrypt

--- code/misc/src/test/java/cryptography/BouncyCastleTest.java
+++ code/misc/src/test/java/cryptography/BouncyCastleTest.java
@@ -2,8 +2,11 @@ package cryptography;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.security.InvalidKeyException;
 import java.security.Key;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.security.Security;
 
@@ -12,17 +15,106 @@ import javax.crypto.CipherOutputStream;
 import javax.crypto.KeyGenerator;
 
 import org.bouncycastle.crypto.InvalidCipherTextException;
+import org.bouncycastle.crypto.digests.SHA256Digest;
 import org.bouncycastle.crypto.engines.AESEngine;
+import org.bouncycastle.crypto.generators.SCrypt;
+import org.bouncycastle.crypto.macs.HMac;
 import org.bouncycastle.crypto.modes.EAXBlockCipher;
 import org.bouncycastle.crypto.params.AEADParameters;
 import org.bouncycastle.crypto.params.KeyParameter;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.bouncycastle.util.Arrays;
+import org.bouncycastle.util.encoders.Base64;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class BouncyCastleTest {
 	@Test
-	public void test() throws InvalidKeyException, IOException {
+	public void testScryptEncrypt() throws NoSuchAlgorithmException, IOException {
+		SecureRandom srng = new SecureRandom(new byte[] { 0 });
+		byte[] S = new byte[32];
+		srng.nextBytes(S);
+
+		byte[] P = "password".getBytes();
+		int logN = 16;
+		int N = 1 << logN;
+		int r = 8;
+		int p = 1;
+		int dkLen = 256;
+		byte[] result = SCrypt.generate(P, S, N, r, p, dkLen);
+		/**
+		 *
+ bytes 0-5: The word "scrypt"
+ byte 6: 0
+ byte 7: logN
+ bytes 8-11: r
+ bytes 12-15: p
+ bytes 16-47: salt (which is 32 bytes)
+ bytes 48-80: A 32 byte SHA256 checksum (hash) of the contents of bytes 0 to 47
+ bytes 81-113: A 32 byte HMAC hash of bytes 0 to 80 with the key being the scrypt cryptographic hash
+		 */
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		baos.write("scrypt".getBytes());
+		baos.write(0);
+		baos.write(logN);
+		baos.write(ByteBuffer.allocate(4).putInt(r).array());
+		baos.write(ByteBuffer.allocate(4).putInt(p).array());
+		baos.write(S);
+
+		byte[] bytes = baos.toByteArray();
+
+		MessageDigest md = MessageDigest.getInstance("SHA-256");
+		md.update(bytes);
+		byte[] digest = md.digest();
+
+		baos.write(digest);
+
+		bytes = baos.toByteArray();
+
+		HMac hmac = new HMac(new SHA256Digest());
+		hmac.init(new KeyParameter(result));
+		hmac.update(bytes, 0, bytes.length);
+
+		byte[] scrypt = new byte[32];
+		hmac.doFinal(scrypt, 0);
+
+		baos.write(scrypt);
+		System.out.println(Base64.toBase64String(baos.toByteArray()));
+	}
+
+	@Test
+	public void testScryptVerify() throws NoSuchAlgorithmException {
+		byte[] P = "password".getBytes();
+		String ciphertext = "c2NyeXB0ABAAAAAIAAAAAXq43IRWwl8TJVHxV8d6GIjvkY+szO7URnUVfM4qkZ+SK5RvP6gl2OTAQM72q5u7omvX6afamageSaAvsP0qJnW7ypEzkfGI0djQLlte9xZakCt16RgeFq2F8t2NV+LaHg==";
+		byte[] bytes = Base64.decode(ciphertext);
+
+		int logN = bytes[7];
+		int N = 1 << logN;
+		int r = ByteBuffer.wrap(Arrays.copyOfRange(bytes, 8, 12)).getInt();
+		int p = ByteBuffer.wrap(Arrays.copyOfRange(bytes, 12, 16)).getInt();
+		byte[] S = Arrays.copyOfRange(bytes, 16, 48);
+		int dkLen = 256;
+
+		MessageDigest md = MessageDigest.getInstance("SHA-256");
+		md.update(Arrays.copyOfRange(bytes, 0, 48));
+		byte[] digest = md.digest();
+
+		Assert.assertTrue(Arrays.areEqual(Arrays.copyOfRange(bytes, 48, 80), digest));
+
+		byte[] result = SCrypt.generate(P, S, N, r, p, dkLen);
+
+		HMac hmac = new HMac(new SHA256Digest());
+		hmac.init(new KeyParameter(result));
+		hmac.update(bytes, 0, 80);
+
+		byte[] scrypt = new byte[32];
+		hmac.doFinal(scrypt, 0);
+
+		Assert.assertTrue(Arrays.areEqual(Arrays.copyOfRange(bytes, 80, bytes.length), scrypt));
+	}
+
+	@Test
+	public void testSymmetricEncrypt() throws InvalidKeyException, IOException {
 		Security.addProvider(new BouncyCastleProvider());
 
 		/*
@@ -91,37 +183,6 @@ public class BouncyCastleTest {
 		int resultLen = eaxCipher.processBytes(intrDat, 0, outOff, datOut, 0);
 		eaxCipher.doFinal(datOut, resultLen);
 
-		if (!areEqual(datIn, datOut)) {
-			fail("EAX roundtrip failed to match");
-		}
-	}
-
-	void fail(String message) {
-		throw new RuntimeException(message);
-	}
-
-	boolean areEqual(byte[] a, byte[] b) {
-		return Arrays.areEqual(a, b);
-	}
-
-	boolean areEqual(byte[][] left, byte[][] right) {
-		if (left == null && right == null) {
-			return true;
-		} else if (left == null || right == null) {
-			return false;
-		}
-
-		if (left.length != right.length) {
-			return false;
-		}
-
-		for (int t = 0; t < left.length; t++) {
-			if (areEqual(left[t], right[t])) {
-				continue;
-			}
-			return false;
-		}
-
-		return true;
+		Assert.assertTrue(Arrays.areEqual(datIn, datOut));
 	}
 }
