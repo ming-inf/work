597edd3836904f2f2f9dd5311fde2e0006e58980
Ming Wong <ming.wong@rbccm.com>
2018-01-30 11:27:52 -0500

    add binary tree

---
+++ code/misc/src/main/java/structure/BinaryTree.java
@@ -0,0 +1,283 @@
+package structure;
+
+import static java.util.Objects.isNull;
+import static java.util.Objects.nonNull;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
+
+
+public class BinaryTree<T extends Comparable<T>> implements Tree<T> {
+	private Node<T> root;
+
+	public BinaryTree() {
+	}
+
+	public BinaryTree(Node<T> root) {
+		this.root = root;
+	}
+
+	public boolean contains(T value) {
+		if (isNull(value)) {
+			return false;
+		}
+		return nonNull(searchNode(null, root, new Node<>(value)).target);
+	}
+
+	private Tuple<Node<T>, Node<T>> searchNode(Node<T> parent, Node<T> current, Node<T> value) {
+		if (isNull(current)) {
+			return new Tuple<>(null, null);
+		}
+
+		if (0 == current.compareTo(value)) {
+			return new Tuple<>(parent, current);
+		} else {
+			Tuple<Node<T>, Node<T>> result = searchNode(current, current.left, value);
+			if (nonNull(result.target)) {
+				return result;
+			}
+
+			result = searchNode(current, current.right, value);
+			if (nonNull(result.target)) {
+				return result;
+			}
+
+			return new Tuple<>(null, null);
+		}
+	}
+
+	public void add(T value) {
+		if (isNull(value)) {
+			return;
+		}
+
+		Node<T> newNode = new Node<>(value);
+		if (isNull(root)) {
+			root = newNode;
+		} else {
+			insertNode(root, newNode);
+		}
+	}
+
+	private void insertNode(Node<T> tree, Node<T> value) {
+		Queue<Node<T>> q = new LinkedList<Node<T>>();
+		q.add(tree);
+		
+		while(!q.isEmpty()) {
+			Node<T> current = q.remove();
+			
+			if (isNull(current.left)) {
+				current.left = value;
+				break;
+			} else {
+				q.add(current.left);
+			}
+			
+			if (isNull(current.right)) {
+				current.right = value;
+				break;
+			} else {
+				q.add(current.right);
+			}
+		}
+	}
+
+	public boolean remove(T value) {
+		if (isNull(value) || isNull(root)) {
+			return false;
+		}
+
+		Tuple<Node<T>, Node<T>> parentCurrent = searchNode(null, root, new Node<>(value));
+		Node<T> parent = parentCurrent.parent;
+		Node<T> target = parentCurrent.target;
+		if (isNull(target)) {
+			return false;
+		}
+
+		Node<T> left = target.left;
+		Node<T> right = target.right;
+
+		boolean isParentNull = isNull(parent);
+		boolean isBothNull = isNull(left) && isNull(right);
+		boolean isLeftOnly = nonNull(left) && isNull(right);
+		boolean isRightOnly = isNull(left) && nonNull(right);
+		boolean isBothNonNull = nonNull(left) && nonNull(right);
+
+		if (!isParentNull) {
+			boolean isLeftChild = parent.left == target;
+			Node<T> newChild;
+			if (isBothNull) {
+				newChild = null;
+			} else if (isLeftOnly) {
+				newChild = left;
+			} else if (isRightOnly) {
+				newChild = right;
+			} else {
+				newChild = isLeftChild ? parent.left : parent.right;
+			}
+
+			if (isLeftChild) {
+				parent.left = newChild;
+			} else {
+				parent.right = newChild;
+			}
+		} else {
+			if (isBothNull) {
+				root = null;
+			} else if (isLeftOnly) {
+				root.value = left.value;
+				left = null;
+			} else if (isRightOnly) {
+				root.value = right.value;
+				right = null;
+			}
+		}
+
+		if (isBothNonNull) {
+			Node<T> largest = left;
+			while (nonNull(largest.right)) {
+				largest = largest.right;
+			}
+			Node<T> largestParent = searchNode(target, target.left, largest).parent;
+			if (largestParent != target) {
+				largestParent.right = null;
+			}
+			target.value = largest.value;
+		}
+
+		return true;
+	}
+
+	@Override
+	public List<T> preorder() {
+		return preorder(root);
+	}
+
+	private List<T> preorder(Node<T> current) {
+		if (isNull(current)) {
+			return Collections.emptyList();
+		}
+
+		List<T> l = new ArrayList<>();
+		l.add(current.value);
+		l.addAll(preorder(current.left));
+		l.addAll(preorder(current.right));
+		return l;
+	}
+
+	@Override
+	public List<T> postorder() {
+		return postorder(root);
+	}
+
+	private List<T> postorder(Node<T> current) {
+		if (isNull(current)) {
+			return Collections.emptyList();
+		}
+
+		List<T> l = new ArrayList<>();
+		l.addAll(postorder(current.left));
+		l.addAll(postorder(current.right));
+		l.add(current.value);
+		return l;
+	}
+
+	@Override
+	public List<T> inorder() {
+		return inorder(root);
+	}
+
+	private List<T> inorder(Node<T> current) {
+		if (isNull(current)) {
+			return Collections.emptyList();
+		}
+
+		List<T> l = new ArrayList<>();
+		l.addAll(inorder(current.left));
+		l.add(current.value);
+		l.addAll(inorder(current.right));
+		return l;
+	}
+
+	@Override
+	public List<T> breadthFirst() {
+		return breadthFirst(root);
+	}
+
+	private List<T> breadthFirst(Node<T> current) {
+		if (isNull(current)) {
+			return Collections.emptyList();
+		}
+
+		List<T> l = new ArrayList<>();
+
+		Queue<Node<T>> q = new ArrayBlockingQueue<>(4);
+		q.add(current);
+		while (!q.isEmpty()) {
+			Node<T> n = q.remove();
+			l.add(n.value);
+			if (nonNull(n.left)) {
+				q.add(n.left);
+			}
+
+			if (nonNull(n.right)) {
+				q.add(n.right);
+			}
+		}
+
+		return l;
+	}
+
+	public static class Node<S extends Comparable<S>> implements Comparable<Node<S>> {
+		S value;
+		Node<S> left;
+		Node<S> right;
+
+		public Node(S value) {
+			this(value, null, null);
+		}
+
+		public Node(S value, Node<S> left, Node<S> right) {
+			this.value = value;
+			this.left = left;
+			this.right = right;
+		}
+
+		@Override
+		public int compareTo(Node<S> o) {
+			return value.compareTo(o.value);
+		}
+
+		@Override
+		public String toString() {
+			return "Node [value=" + value + ", left=" + left + ", right=" + right + "]";
+		}
+	}
+
+	private static class Tuple<X, Y> {
+		public X parent;
+		public Y target;
+
+		public Tuple(X x, Y y) {
+			this.parent = x;
+			this.target = y;
+		}
+	}
+
+	@Override
+	public int height() {
+		return heightNode(root);
+	}
+
+	public int heightNode(Node<T> current) {
+		if (isNull(current)) {
+			return 0;
+		} else {
+			return 1 + Math.max(heightNode(current.left), heightNode(current.right));
+		}
+	}
+}
---
+++ code/misc/src/test/java/structure/BinaryTreeTest.java
@@ -0,0 +1,355 @@
+package structure;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class BinaryTreeTest {
+	private Tree<Integer> objectUnderTest;
+
+	@Before
+	public void setup() {
+		objectUnderTest = new BinaryTree<>();
+	}
+
+	@Test
+	public void testCustomTree() {
+		BinaryTree.Node<Integer> root = new BinaryTree.Node<Integer>(0);
+		BinaryTree.Node<Integer> left = new BinaryTree.Node<Integer>(1);
+		BinaryTree.Node<Integer> right = new BinaryTree.Node<Integer>(2);
+		BinaryTree.Node<Integer> rightRight = new BinaryTree.Node<Integer>(3);
+		right.right = rightRight;
+		root.left = left;
+		root.right = right;
+		
+		objectUnderTest = new BinaryTree<Integer>(root);
+		
+		assertEquals(Arrays.asList(0, 1, 2, 3), objectUnderTest.preorder());
+		assertEquals(Arrays.asList(1, 3, 2, 0), objectUnderTest.postorder());
+		assertEquals(Arrays.asList(1, 0, 2, 3), objectUnderTest.inorder());
+		assertEquals(Arrays.asList(0, 1, 2, 3), objectUnderTest.breadthFirst());
+	}
+
+	@Test
+	public void testSearchNull() {
+		assertFalse(objectUnderTest.contains(null));
+	}
+
+	@Test
+	public void testSearchNotFoundEmpty() {
+		assertFalse(objectUnderTest.contains(0));
+	}
+
+	@Test
+	public void testSearchNotFound() {
+		objectUnderTest.add(0);
+
+		assertFalse(objectUnderTest.contains(1));
+	}
+
+	@Test
+	public void testSearchFound() {
+		objectUnderTest.add(0);
+
+		assertTrue(objectUnderTest.contains(0));
+	}
+
+	@Test
+	public void testSearchNotFoundLeft() {
+		objectUnderTest.add(0);
+		objectUnderTest.add(-1);
+
+		assertFalse(objectUnderTest.contains(-2));
+	}
+
+	@Test
+	public void testSearchNotFoundRight() {
+		objectUnderTest.add(0);
+		objectUnderTest.add(1);
+
+		assertFalse(objectUnderTest.contains(2));
+	}
+
+	@Test
+	public void testSearchNotFoundBoth() {
+		objectUnderTest.add(0);
+		objectUnderTest.add(-1);
+		objectUnderTest.add(1);
+
+		assertFalse(objectUnderTest.contains(-2));
+	}
+
+	@Test
+	public void testInsertNull() {
+		objectUnderTest.add(null);
+	}
+
+	@Test
+	public void testInsert() {
+		objectUnderTest.add(0);
+
+		assertFalse(objectUnderTest.contains(-1));
+		assertTrue(objectUnderTest.contains(0));
+		assertFalse(objectUnderTest.contains(1));
+	}
+
+	@Test
+	public void testInsertFull() {
+		Integer[] intArray = { 0, 1, 2, 3, 4, 5, 6 };
+		List<Integer> ints = Arrays.asList(intArray);
+
+		for (int i : ints) {
+			objectUnderTest.add(i);
+		}
+
+		assertEquals(Arrays.asList(0, 1, 3, 4, 2, 5, 6), objectUnderTest.preorder());
+		assertEquals(Arrays.asList(3, 4, 1, 5, 6, 2, 0), objectUnderTest.postorder());
+		assertEquals(Arrays.asList(3, 1, 4, 0, 5, 2, 6), objectUnderTest.inorder());
+		assertEquals(Arrays.asList(0, 1, 2, 3, 4, 5, 6), objectUnderTest.breadthFirst());
+	}
+
+	@Test
+	public void testDeleteNull() {
+		assertFalse(objectUnderTest.remove(null));
+	}
+
+	@Test
+	public void testDeleteEmpty() {
+		assertFalse(objectUnderTest.remove(0));
+	}
+
+	@Test
+	public void testDeleteNotFound() {
+		objectUnderTest.add(0);
+
+		assertFalse(objectUnderTest.remove(1));
+	}
+
+	@Test
+	public void testDeleteRootLeaf() {
+		objectUnderTest.add(0);
+
+		assertTrue(objectUnderTest.remove(0));
+		assertFalse(objectUnderTest.contains(0));
+	}
+
+	@Test
+	public void testDeleteRootWithLeft() {
+		objectUnderTest.add(0);
+		objectUnderTest.add(-1);
+
+		assertTrue(objectUnderTest.remove(0));
+		assertFalse(objectUnderTest.contains(0));
+	}
+
+	@Test
+	public void testDeleteRootWithRight() {
+		objectUnderTest.add(0);
+		objectUnderTest.add(1);
+
+		assertTrue(objectUnderTest.remove(0));
+		assertFalse(objectUnderTest.contains(0));
+	}
+
+	@Test
+	public void testDeleteRootWithBoth() {
+		objectUnderTest.add(0);
+		objectUnderTest.add(1);
+		objectUnderTest.add(-1);
+
+		assertTrue(objectUnderTest.remove(0));
+		assertFalse(objectUnderTest.contains(0));
+		assertTrue(objectUnderTest.contains(1));
+		assertTrue(objectUnderTest.contains(-1));
+	}
+
+	@Test
+	public void testDeleteNonRootLeftLeaf() {
+		objectUnderTest.add(0);
+		objectUnderTest.add(-1);
+
+		assertTrue(objectUnderTest.remove(-1));
+		assertFalse(objectUnderTest.contains(-1));
+	}
+
+	@Test
+	public void testDeleteNonRootLeftWithLeft() {
+		objectUnderTest.add(0);
+		objectUnderTest.add(-1);
+		objectUnderTest.add(-2);
+
+		assertTrue(objectUnderTest.remove(-1));
+		assertFalse(objectUnderTest.contains(-1));
+		assertTrue(objectUnderTest.contains(-2));
+	}
+
+	@Test
+	public void testDeleteNonRootLeftWithRight() {
+		objectUnderTest.add(0);
+		objectUnderTest.add(-2);
+		objectUnderTest.add(-1);
+
+		assertTrue(objectUnderTest.remove(-2));
+		assertFalse(objectUnderTest.contains(-2));
+		assertTrue(objectUnderTest.contains(-1));
+	}
+
+	@Test
+	public void testDeleteNonRootLeftWithBoth() {
+		objectUnderTest.add(0);
+		objectUnderTest.add(-2);
+		objectUnderTest.add(-3);
+		objectUnderTest.add(-1);
+
+		assertTrue(objectUnderTest.remove(-2));
+		assertFalse(objectUnderTest.contains(-2));
+		assertTrue(objectUnderTest.contains(-1));
+		assertTrue(objectUnderTest.contains(-3));
+	}
+
+	@Test
+	public void testDeleteNonRootRightLeaf() {
+		objectUnderTest.add(0);
+		objectUnderTest.add(1);
+
+		assertTrue(objectUnderTest.remove(1));
+		assertFalse(objectUnderTest.contains(1));
+	}
+
+	@Test
+	public void testDeleteNonRootRightWithLeft() {
+		objectUnderTest.add(0);
+		objectUnderTest.add(2);
+		objectUnderTest.add(1);
+
+		assertTrue(objectUnderTest.remove(2));
+		assertFalse(objectUnderTest.contains(2));
+		assertTrue(objectUnderTest.contains(1));
+	}
+
+	@Test
+	public void testDeleteNonRootRightWithRight() {
+		objectUnderTest.add(0);
+		objectUnderTest.add(1);
+		objectUnderTest.add(2);
+
+		assertTrue(objectUnderTest.remove(1));
+		assertFalse(objectUnderTest.contains(1));
+		assertTrue(objectUnderTest.contains(2));
+	}
+
+	@Test
+	public void testDeleteNonRootRightWithBoth() {
+		objectUnderTest.add(0);
+		objectUnderTest.add(2);
+		objectUnderTest.add(1);
+		objectUnderTest.add(3);
+
+		assertTrue(objectUnderTest.remove(2));
+		assertFalse(objectUnderTest.contains(2));
+		assertTrue(objectUnderTest.contains(1));
+		assertTrue(objectUnderTest.contains(3));
+	}
+
+	@Test
+	public void testDeleteNonRootLeft() {
+		objectUnderTest.add(0);
+		objectUnderTest.add(-5);
+		objectUnderTest.add(-8);
+		objectUnderTest.add(-7);
+		objectUnderTest.add(-4);
+
+		assertTrue(objectUnderTest.remove(-5));
+		assertFalse(objectUnderTest.contains(-5));
+		assertTrue(objectUnderTest.contains(-8));
+		assertTrue(objectUnderTest.contains(-7));
+	}
+
+	@Test
+	public void testTraversePreorder() {
+		Integer[] intArray = { 23, 14, 7, 9, 17, 31 };
+		List<Integer> ints = Arrays.asList(intArray);
+
+		for (int i : ints) {
+			objectUnderTest.add(i);
+		}
+
+		List<Integer> expected = Arrays.asList(23, 14, 9, 17, 7 ,31);
+		List<Integer> actualIntArray = objectUnderTest.preorder();
+
+		assertEquals(expected, actualIntArray);
+	}
+
+	@Test
+	public void testTraversePostorder() {
+		Integer[] intArray = { 23, 14, 7, 9, 17, 31 };
+		List<Integer> ints = Arrays.asList(intArray);
+
+		for (int i : ints) {
+			objectUnderTest.add(i);
+		}
+
+		List<Integer> expected = Arrays.asList(9, 17, 14, 31, 7, 23);
+		List<Integer> actualIntArray = objectUnderTest.postorder();
+
+		assertEquals(expected, actualIntArray);
+	}
+
+	@Test
+	public void testTraverseInorder() {
+		Integer[] intArray = { 23, 14, 7, 9, 17, 31 };
+		List<Integer> ints = Arrays.asList(intArray);
+
+		for (int i : ints) {
+			objectUnderTest.add(i);
+		}
+
+		List<Integer> expected = Arrays.asList(9, 14, 17, 23, 31, 7);
+		List<Integer> actualIntArray = objectUnderTest.inorder();
+
+		assertEquals(expected, actualIntArray);
+	}
+
+	@Test
+	public void testTraverseBreadthFirstNull() {
+		List<Integer> expected = Collections.emptyList();
+		List<Integer> actualIntArray = objectUnderTest.breadthFirst();
+
+		assertEquals(expected, actualIntArray);
+	}
+
+	@Test
+	public void testTraverseBreadthFirst() {
+		Integer[] intArray = { 23, 14, 7, 9, 17, 31 };
+		List<Integer> ints = Arrays.asList(intArray);
+
+		for (int i : ints) {
+			objectUnderTest.add(i);
+		}
+
+		List<Integer> expected = Arrays.asList(23, 14, 7, 9, 17, 31);
+		List<Integer> actualIntArray = objectUnderTest.breadthFirst();
+
+		assertEquals(expected, actualIntArray);
+	}
+
+	@Test
+	public void testHeight() {
+		objectUnderTest.add(0);
+		objectUnderTest.add(-2);
+		objectUnderTest.add(-1);
+		objectUnderTest.add(-3);
+		objectUnderTest.add(2);
+		objectUnderTest.add(1);
+		objectUnderTest.add(3);
+
+		assertEquals(3, objectUnderTest.height());
+	}
+}
