0156e78a7546b98414d75d5a8588ea1481742506
ming wong <ming.wong@avanade.com>
2018-02-02 15:14:28 -0500

    refactor

--- code/misc/src/test/java/practice/KeyPlaintextRunningKeyEncryptionTest.java
+++ code/misc/src/test/java/practice/KeyPlaintextRunningKeyEncryptionTest.java
@@ -3,23 +3,26 @@ package practice;
 import java.util.Arrays;
 import java.util.List;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class KeyPlaintextRunningKeyEncryptionTest {
 	@Test
 	public void test() {
 		List<String> lines = Arrays.asList("SGZVQBUQAFRWSLC", "ACM");
+		List<String> expected = Arrays.asList("SENDMOREMONKEYS");
 
 		if (0 == lines.size() % 2) {
 			for (int i = 0; i < lines.size(); i += 2) {
 				String ciphertext = lines.get(i);
 				String key = lines.get(i + 1);
-				System.out.println(String.format("%s %s -> %s", ciphertext, key, KeyPlaintextRunningKeyEncryption.decrypt(key, ciphertext)));
+
+				String plaintext = KeyPlaintextRunningKeyEncryption.decrypt(key, ciphertext);
+				Assert.assertEquals(expected.get(i / 2), plaintext);
+
+				String encryptPlaintext = KeyPlaintextRunningKeyEncryption.encrypt(key, plaintext);
+				Assert.assertEquals(ciphertext, encryptPlaintext);
 			}
 		}
-
-		String plaintext = "SENDMOREMONKEYS";
-		String key = "ACM";
-		System.out.println(String.format("%s %s -> %s", plaintext, key, KeyPlaintextRunningKeyEncryption.encrypt(key, plaintext)));
 	}
 }
