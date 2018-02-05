b44a1d9c442fed2f060e7259d90e34377dee1069
ming wong <ming.wong@avanade.com>
2018-02-05 16:09:36 -0500

    add list of first 10000 primes in serialized form

---
+++ code/misc/src/main/java/math/prime/First10000.java
@@ -0,0 +1,39 @@
+package math.prime;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import util.Log;
+
+public class First10000 implements Log {
+	public int[] deserialize() throws FileNotFoundException, IOException, ClassNotFoundException {
+		String filename = "10000primes.ser";
+
+		try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(filename))) {
+			int[] primes = (int[]) in.readObject();
+			return primes;
+		}
+	}
+
+	public void print(int[] primes) {
+		for (int p : primes) {
+			i(p);
+		}
+	}
+
+	public static void main(String[] args) throws IOException, ClassNotFoundException {
+		First10000 o = new First10000();
+		int[] primes = o.deserialize();
+		o.print(primes);
+	}
+
+	@Override
+	public Logger getLogger() {
+		return LogManager.getLogger();
+	}
+}
