c4d531c48f91ae99d049f283a3b17404c4747657
ming wong <ming.wong@avanade.com>
2018-02-16 11:25:14 -0500

    rename GraphNode to TreeNode

--- code/misc/src/main/java/algorithm/TreeNaturalNumber.java
+++ code/misc/src/main/java/algorithm/TreeNaturalNumber.java
@@ -8,25 +8,25 @@ import java.util.List;
 import static java.util.Objects.isNull;
 
 public class TreeNaturalNumber {
-  private GraphNode[] trees;
+  private TreeNode[] trees;
   private First10000 primes;
 
   TreeNaturalNumber() {
-    trees = new GraphNode[10000];
+    trees = new TreeNode[10000];
     primes = new First10000();
   }
 
   public void init() {
-    trees[0] = new GraphNode("2");
+    trees[0] = new TreeNode("2");
     for (int i = 1; i < trees.length; i++) {
       int j = primes.isPrime(i + 1);
       if (-1 < j) {
-        GraphNode n = new GraphNode("" + (i + 1));
+        TreeNode n = new TreeNode("" + (i + 1));
         n.addEdge(trees[j]);
         trees[i] = n;
       } else {
         List<Integer> factors = First10000.primeFactors(i + 1);
-        GraphNode n = new GraphNode((i + 1) + "");
+        TreeNode n = new TreeNode((i + 1) + "");
         for (int factor : factors) {
           n.addEdge(trees[factor - 2]);
         }
@@ -44,27 +44,27 @@ public class TreeNaturalNumber {
     return -1 < isPrime ? 1 + isPrime : -1;
   }
 
-  public GraphNode t(int n) {
+  public TreeNode t(int n) {
     if (1 == n) {
       return null;
     } else if (2 == n) {
       if (isNull(trees[n - 2])) {
-        trees[n - 2] = new GraphNode("2");
+        trees[n - 2] = new TreeNode("2");
       }
       return trees[n - 2];
     }
 
-    GraphNode node = new GraphNode("" + n);
+    TreeNode node = new TreeNode("" + n);
     int isPrime = primes.isPrime(n);
     if (0 < isPrime) {
-      GraphNode prime = trees[n];
+      TreeNode prime = trees[n];
       node.addEdge(isNull(prime) ? t(pInv(n)) : prime);
       trees[n - 2] = node;
       return node;
     } else {
       List<Integer> factors = First10000.primeFactors(n);
       for (int factor : factors) {
-        GraphNode t = trees[factor - 2];
+        TreeNode t = trees[factor - 2];
         if (isNull(t)) {
           t = t(factor);
         }
@@ -75,7 +75,7 @@ public class TreeNaturalNumber {
     return node;
   }
 
-  public int tInv(GraphNode t) {
+  public int tInv(TreeNode t) {
     if (isNull(t)) {
       return 1;
     } else if (t.connectedTo.isEmpty()) {
@@ -85,21 +85,21 @@ public class TreeNaturalNumber {
     }
 
     int i = 1;
-    for (GraphNode child : t.connectedTo) {
+    for (TreeNode child : t.connectedTo) {
       i *= tInv(child);
     }
     return i;
   }
 
-  public static class GraphNode {
+  public static class TreeNode {
     String id;
-    List<GraphNode> connectedTo = new ArrayList<>();
+    List<TreeNode> connectedTo = new ArrayList<>();
 
-    GraphNode(String id) {
+    TreeNode(String id) {
       this.id = id;
     }
 
-    void addEdge(GraphNode target) {
+    void addEdge(TreeNode target) {
       connectedTo.add(target);
     }
 
